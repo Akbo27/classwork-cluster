0bee90f7455e35a5ac7959cd4d7a4542497ca2a3
Akbo27 <aungkaungbo.edu@gmail.com>
2025-01-24 10:34:50 +0700

    Minor Adjustments

--- classwork.py
+++ classwork.py
@@ -13,7 +13,9 @@ with open(file_path, 'r') as file:
 
 points = np.array(points)
 
-kmeans = KMeans(n_clusters=3, random_state=0)
+num_clusters = 3
+
+kmeans = KMeans(num_clusters, random_state=0)
 kmeans.fit(points)
 centroids_kmeans = kmeans.cluster_centers_
 labels = kmeans.labels_
@@ -23,7 +25,7 @@ x_max, y_max = points.max(axis=0)
 
 plt.figure(figsize=(8, 6))
 
-for i in range(3):
+for i in range(num_clusters):
     plt.scatter(points[labels == i][:, 0], points[labels == i][:, 1], label=f'Cluster {i+1}')
 
 plt.scatter(centroids_kmeans[:, 0], centroids_kmeans[:, 1], color='red', marker='x', s=100, label='K-means Centroids')
@@ -33,7 +35,7 @@ plt.axvline(x=x_max, color='green', linestyle='--')
 plt.axhline(y=y_min, color='purple', linestyle='--', label='Y Bounds')
 plt.axhline(y=y_max, color='purple', linestyle='--')
 
-plt.title('Points and K-means Estimated Centroids')
+plt.title('Class entertainment - Guess the clusters')
 plt.xlabel('X Coordinate')
 plt.ylabel('Y Coordinate')
 plt.legend()
